69b94add519e49ec973fdaca513778519144823b
jarvis <aly.pezzutti@gmail.com>
2024-08-02 11:03:23 +0200

    Project skeleton ok commit

---
+++ website/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'djskdhkgh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config["CACHE_TYPE"] = "null"
+    db.init_app(app)
+
+    # import the name of the blueprint
+    from .views import views 
+
+    # register blueprint w the flask app
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import Transactions
+
+    with app.app_context():
+        db.create_all()
+    return app
+    
---
+++ website/views.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, render_template, request, redirect, session, flash, json, jsonify
+from .models import Transactions
+from . import db
+from datetime import datetime
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+def home():
+    history = Transactions.query.order_by(Transactions.id.desc()).all()
+    data = request.form
+    # print(data)
+    if request.method == 'POST':
+        flag = request.form.get('flag')
+        date = request.form.get('date')
+        tdate = datetime.strptime(date, '%Y-%m-%d').date()
+        category = request.form.get('category')
+        amount = float(request.form.get('amount'))
+        if flag == 'out':
+            amount = -amount
+        description = request.form.get('description')
+
+        new_transaction = Transactions(date=tdate, category=category, amount=amount, description=description, flag=flag)
+
+        db.session.add(new_transaction)
+        db.session.commit()
+        flash('Transaction inserted!', category='success')
+
+        history = Transactions.query.order_by(Transactions.id.desc()).all()
+
+        return render_template("home.html", history=history)
+    
+    return render_template("home.html", history=history)
+
+@views.route('/stats')
+def stats():
+    return render_template("stats.html")
+
+@views.route('/income')
+def income():
+    return render_template("income.html")
+
+@views.route('/expenses')
+def expenses():
+    return render_template("expenses.html")
+
+
+@views.route('/delete-row', methods=['POST'])
+def delete_row():
+    row = json.loads(request.data)
+    rowId = row['rowId']
+    row = Transactions.query.get(rowId)
+    
+    if row:
+        db.session.delete(row)
+        db.session.commit()
+
+    flash('row deleted...', category='success')
+
+    history = Transactions.query.order_by(Transactions.id.desc()).all()
+
+    return jsonify({})
+
+@views.route('/edit')
+def edit():
+    history = Transactions.query.order_by(Transactions.id.desc()).all()
+    return render_template("edit.html", history=history)
